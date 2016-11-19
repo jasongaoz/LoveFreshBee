b7085bd5e3e94b5b306fdb396265fd7dd9fb3d70
Colbert Zhan <ming__ming@163.com>
2016-11-19 19:21:07 +0800

    创建闪电超市右边的cell

---
+++ LoveFreshBee/LoveFreshBee/Class/SuperMarket/Views/AFBOrderRightCell.m
@@ -0,0 +1,24 @@
+//
+//  AFBOrderRightCell.m
+//  LoveFreshBee
+//
+//  Created by Colbert Zhan on 2016/11/19.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import "AFBOrderRightCell.h"
+
+@implementation AFBOrderRightCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ LoveFreshBee/LoveFreshBee/Class/SuperMarket/Views/AFBOrderRightTableView.m
@@ -0,0 +1,21 @@
+//
+//  AFBOrderRightTableView.m
+//  LoveFreshBee
+//
+//  Created by Colbert Zhan on 2016/11/19.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import "AFBOrderRightTableView.h"
+
+@implementation AFBOrderRightTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
