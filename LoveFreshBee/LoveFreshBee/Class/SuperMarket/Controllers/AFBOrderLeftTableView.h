97457f23f3238476978e3ce372904a4706d78b2a
gao2015 <793130302@qq.com>
2016-11-19 17:04:24 +0800

    测试tableView

---
+++ LoveFreshBee/LoveFreshBee/Class/SuperMarket/Controllers/AFBOrderLeftTableView.h
@@ -0,0 +1,14 @@
+//
+//  AFBOrderLeftTableView.h
+//  LoveFreshBee
+//
+//  Created by gao2015 on 2016/11/19.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFBOrderLeftTableView : UITableView
+
+
+@end
---
+++ LoveFreshBee/LoveFreshBee/Class/SuperMarket/Controllers/AFBOrderLeftTableView.m
@@ -0,0 +1,27 @@
+//
+//  AFBOrderLeftTableView.m
+//  LoveFreshBee
+//
+//  Created by gao2015 on 2016/11/19.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import "AFBOrderLeftTableView.h"
+
+@implementation AFBOrderLeftTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (void)setupUI{
+    
+
+
+
+}
+
+@end
