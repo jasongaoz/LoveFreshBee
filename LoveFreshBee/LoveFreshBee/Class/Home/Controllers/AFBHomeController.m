c49168d99b75c1437fb86cbcdf7dc8b5e1de9cc6
王志华 <zhihua@zhangzhedeMacBook-Pro.local>
2016-11-20 09:18:28 +0800

    添加了首页NavagationItem

--- LoveFreshBee/LoveFreshBee/Class/Home/Controllers/AFBHomeController.m
+++ LoveFreshBee/LoveFreshBee/Class/Home/Controllers/AFBHomeController.m
@@ -40,6 +40,33 @@
     headerView.frame = CGRectMake(0, 0, 0, 200);
     
     mainTableView.tableHeaderView = headerView;
+    
+    
+    //MARK:添加NavigationItem
+    [self addNavigationItem];
+}
+
+#pragma mark - 添加 设置NavigationItem
+- (void)addNavigationItem{
+    //左侧
+    //不让系统渲染图片
+    UIImage *leftImage = [[UIImage imageNamed:@"icon_black_scancode"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIBarButtonItem * leftItem = [[UIBarButtonItem alloc]initWithImage:leftImage style:UIBarButtonItemStylePlain target:self action:@selector(clickLeftItem)];
+    self.navigationItem.leftBarButtonItem = leftItem;
+    
+    //右侧
+    //不让系统渲染图片
+    UIImage *rightImage = [[UIImage imageNamed:@"icon_search"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIBarButtonItem * rightItem = [[UIBarButtonItem alloc]initWithImage:rightImage style:UIBarButtonItemStylePlain target:self action:@selector(clickRightItem)];
+    self.navigationItem.rightBarButtonItem = rightItem;
+}
+
+- (void)clickLeftItem{
+    NSLog(@"点击了扫一扫");
+}
+
+- (void)clickRightItem{
+    NSLog(@"点击了搜索");
 }
 
 
