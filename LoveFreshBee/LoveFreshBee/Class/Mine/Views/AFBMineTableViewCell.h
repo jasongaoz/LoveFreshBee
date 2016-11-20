e68f570ccf6724d7aa5747396c4441844d10aeaf
xuliuxiao <1870680466@qq.com>
2016-11-20 11:08:47 +0800

    界面按钮的修改

---
+++ LoveFreshBee/LoveFreshBee/Class/Mine/Views/AFBMineTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AFBMineTableViewCell.h
+//  LoveFreshBee
+//
+//  Created by xlx on 16/11/19.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFBMineTableViewCell : UITableViewCell
+
+@end
